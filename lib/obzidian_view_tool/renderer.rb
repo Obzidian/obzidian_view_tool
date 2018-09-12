60641a30bc19d3898ca0d5cb77595932c6a99718
obzidian <jadelangel87@gmail.com>
2018-09-12 09:17:30 -0500

    Changed named

--- lib/obzidian_view_tool/renderer.rb
+++ lib/obzidian_view_tool/renderer.rb
@@ -1,7 +1,7 @@
 module ObzidianViewTool
   class Renderer
     def self.copyright name, msg
-      "&copy; #{Time.now.year} | <b>#{Jonas del Angel}</b> #{msg}".html_safe
+      "&copy; #{Time.now.year} | <b>#{Obzidian}</b> #{msg}".html_safe
     end
   end
 end
